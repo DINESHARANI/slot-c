d27865b7359d1341a52f47b287ffa8c2f1130fac
DINESHARANI <112246358+DINESHARANI@users.noreply.github.com>
2022-09-27 10:22:52 +0530

    Add files via upload

---
+++ day 11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string n,s;
+	cout<<"enter the password";
+	cin>>n;
+	cout<<"renter the password";
+	cin>>s;
+	if(n==s)
+	cout<<"valid password";
+	else
+	cout<<"invalid password";
+}
